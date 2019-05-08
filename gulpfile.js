1d35ba558bf9049d5de37902202fd95bc359af5f
fant1kua <fant1kua@users.noreply.github.com>
2019-05-08 13:08:16 +0300

    Refactoring

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+const { src, dest, parallel } = require('gulp');
+const less = require('gulp-less');
+const autoprefixer = require('gulp-autoprefixer');
+const minifyCSS = require('gulp-csso');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+
+function js() {
+    return src(['src/js/picker.js', 'src/js/picker.date.js'], { sourcemaps: true })
+        .pipe(uglify())
+        .pipe(concat('pickadate.min.js'))
+        .pipe(dest('dist/js', { sourcemaps: true }))
+}
+
+function css() {
+    return src(['src/css/base.less', 'src/css/base.date.less', 'src/css/default.less'])
+        .pipe(less())
+        .pipe(autoprefixer({
+            browsers: [ '> 5%', 'last 2 versions', 'ie 8', 'ie 9' ],
+            cascade: false
+        }))
+        .pipe(minifyCSS())
+        .pipe(concat('pickadate.css'))
+        .pipe(dest('dist/css'));
+}
+
+exports.js = js;
+exports.css = css;
+exports.default = parallel(js, css);
